d3712b891883e566c7501439a7e18b7caffc8144
dokratos <dokratos@hotmail.it>
2024-02-05 14:49:22 +0100

    testRestTemplate

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,10 +1,23 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
+
+import java.net.http.HttpHeaders;
+import java.util.Arrays;
 
 
 //this will not load the whole context, but use mock object:::
@@ -20,11 +33,53 @@ public class UsersControllerIntegrationTest {
     @LocalServerPort
     private int localServerPort;
 
-    @Test
-    void contextLoads() {
-        System.out.println("server.port " + serverPort);
-        System.out.println("running server port " + localServerPort);
-    }
+    @Autowired
+    private TestRestTemplate testTemplate;
+//
+//    @Test
+//    void contextLoads() {
+//        System.out.println("server.port " + serverPort);
+//        System.out.println("running server port " + localServerPort);
+//    }
 
+    @Test
+    @DisplayName("User can be created")
+    void restCreateUser_whenValidDetailsProvided_returnsUserDetails() {
+        String createUserJSon = "{\n" +
+                " \"firstName\":\"Sergey\",\n" +
+                " \"lastName\":\"Kargopolov\",\n" +
+                " \"email\":\"test@test.it\",\n" +
+                " \"password\":\"123456789\",\n" +
+                " \"repeatPassword\":\"123456789\",\n" +
+                "}";
+
+        JSONObject userDetailRequestJson = new JSONObject();
+        userDetailRequestJson.put("firstName", "Sergey");
+        userDetailRequestJson.put("lastName", "Kargopolov");
+        userDetailRequestJson.put("email", "test@test.it");
+        userDetailRequestJson.put("password", "123456789");
+        userDetailRequestJson.put("repeatPassword", "123456789");
+
+
+//        TODO: check alternative to this code - this does not work!
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> request = new HttpEntity<>(userDetailRequestJson.toString(), headers);
+
+        ResponseEntity<UserRest> createdUserDetailsEntity = testTemplate.postForEntity("/users",
+                request,
+                UserRest.class);
+
+        UserRest createdUserDetails = createdUserDetailsEntity.getBody();
+
+        Assertions.assertEquals(HttpStatus.OK, createdUserDetailsEntity.getStatusCode());
+        Assertions.assertEquals(userDetailRequestJson.getAsString("firstName"), createdUserDetails.getFirstName());
+        Assertions.assertEquals(userDetailRequestJson.getAsString("lastName"), createdUserDetails.getLastName());
+        Assertions.assertEquals(userDetailRequestJson.getAsString("email"), createdUserDetails.getEmail());
+        Assertions.assertFalse(createdUserDetails.getUserId().trim().isEmpty(), "User id should not be empty");
+
+    }
 
 }
